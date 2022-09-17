a7b3b7f92eddf30108e1c3b396832e8e7fb7b80b
vomkaraxioned <omkarv@axioned.com>
2022-09-17 15:30:56 +0530

    form and gallery done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,104 @@
 
 //variables for slider
 const slides = document.querySelector(".feedbacks");
-let sliderControls, slidesLength, sliderLength, i, newControlBtn, activeControl,screenWidth,scrollIterator = currentSliderPosition = 0;
+let sliderControls, slidesLength, sliderLength, i, newControlBtn, activeControl,screenWidth,sliderCall,scrollIterator = currentSliderPosition = 0;
 //variables for footer modal 
 const footerModalControls = document.querySelectorAll("footer .service a");
 const footerModal = document.querySelectorAll("footer .service .dropDown");
 let activeFooterModal;
 //variables for gallery modal
+const galleryItemsHolder =document.querySelector(".gallery .gallery-items");
+const galleryItems = document.querySelectorAll(".gallery .gallery-item");
+let galleryModal,modal,galleryCancel,galleryFigure,galleryImage;
 //variables for form
-const contactForm = document.querySelector(".form-section form");
+const contactForm = document.querySelector(".form-section form[name=contact-form");
+const errorBox = document.querySelectorAll(".err");
+let userName,email,subject,message,valid,emailRe = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/,textRe = /^[a-zA-Z\s]+$/;
 //variables for navbar
+
+
+
+/*=====footer modal end here=====*/
+
+/*=====form validation start here=====*/
+function validation() {
+    valid = true;
+    userName =document.forms["contact-form"]["name"].value;
+    email =document.forms["contact-form"]["email"].value;
+    subject =document.forms["contact-form"]["subject"].value;
+    message =document.forms["contact-form"]["message"].value;
+    try{
+        if(userName > 15 || userName < 2){
+            valid = false;
+            throw "name should be more than 2 letters and less than 15 letters";
+        }else if(userName == ""){
+            valid = false;
+            throw "Please provide name";
+        }else if(!textRe.test(userName)){
+            valid = false;
+            throw "only letters allowed";
+        }else {
+            errorBox[0].style.display = "none";
+        }
+    }catch (e){
+        errorBox[0].innerHTML = e;
+        errorBox[0].style.display = "block";
+    }
+    try{
+      if(email == ""){
+        valid = false;
+            throw "Please provide email";
+        }else if(!emailRe.test(email)){
+            valid = false;
+            throw "Please provide valid email";
+        }else {
+            errorBox[1].style.display = "none";
+        }
+    }catch (e){
+        errorBox[1].innerHTML = e;
+        errorBox[1].style.display = "block";
+    }
+    try{
+        if(subject > 20){
+            valid = false;
+            throw "subject should be less than 20 letters";
+        }else if(subject == ""){
+            valid = false;
+            throw "Please provide subject";
+        }else {
+            errorBox[2].style.display = "none";
+        }
+    }catch (e){
+        errorBox[2].innerHTML = e;
+        errorBox[2].style.display = "block";
+    }
+    try{
+        if(message > 50 ){
+            valid = false;
+            throw "message should less than 50 letters";
+        }else if(message == ""){
+            valid = false;
+            throw "Please provide message";
+        }else {
+            errorBox[3].style.display = "none";
+        }
+    }catch (e){
+        errorBox[3].innerHTML = e;
+        errorBox[3].style.display = "block";
+    }
+    if(valid){
+        alert("We contact you soon"+"\t"+userName);
+        contactForm.reset();
+    }
+}
+if(document.title == "Contact"){
+    clearInterval(sliderCall);
+    contactForm.addEventListener("submit",(e)=>{
+        validation();
+        e.preventDefault();
+    });
+}
+/*=====form validation end here=====*/
 
 /*=====slider start here=====*/
 // adding event listener to control button
@@ -67,9 +156,11 @@ function addSliderControls() {
     }
     addListener();
 }
-addListener();
+if(document.title == "Home"){
+ addListener();
 sliderControls[0].classList.add("control-active");
-setInterval(sliderSliding, 2000);
+sliderCall =setInterval(sliderSliding, 2000);
+}
 /*=====slider ends here=====*/
 
 /*=====footer modal start here=====*/
@@ -88,9 +179,36 @@ footerModalControls.forEach((m,i)=>{
 });
 /*=====footer modal end here=====*/
 
-/*=====form validation start here=====*/
-contactForm.addEventListener("submit",(e)=>{
-    alert("ji");
-    e.preventDefault();
-});
-/*=====form validation end here=====*/
+/*=====gallery modal start here=====*/
+function cancelBtnListener(){
+    galleryCancel = document.querySelector(".gallery-cancel");
+    galleryModal = document.querySelector(".gallery-active-item");
+    galleryCancel.addEventListener("click",()=>{
+        galleryItemsHolder.removeChild(galleryModal);
+    });
+}
+
+function galleryPopUp (item){
+    galleryModal = document.createElement("div");
+    galleryFigure =document.createElement("figure");
+    galleryImage =document.createElement("img");
+    galleryImage.src = item.src;
+    galleryImage.alt = item.alt;
+    galleryCancel = document.createElement("span");
+    galleryModal.classList.add("gallery-active-item");
+    galleryCancel.classList.add("gallery-cancel");
+    galleryFigure.appendChild(galleryImage);
+    galleryModal.appendChild(galleryCancel);
+    galleryModal.appendChild(galleryFigure);
+    galleryItemsHolder.appendChild(galleryModal);
+    cancelBtnListener();
+}
+if(document.title == "Gallery"){
+    galleryItems.forEach((ele)=>{
+        ele.addEventListener("click",()=>{
+            image = ele.children[0].children[0];
+            galleryPopUp(image);
+        });
+    });
+}
+/*=====gallery modal ends here=====*/
